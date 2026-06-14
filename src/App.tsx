8aa61ce899873600d705a2ee93140b4762c14913
Diego Alexandre <elalexdgr@gmail.com>
2026-06-14 20:25:57 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+    <Button variant="destructive">Hello World</Button>
+      <h1>Hello World!</h1>
+    </>
+  );
+}
+
+export default App;
