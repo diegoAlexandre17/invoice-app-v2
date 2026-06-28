459a62df1f42ca5e2541a936500e909faf4a44b2
Diego Alexandre <elalexdgr@gmail.com>
2026-06-28 20:43:07 -0300

    add react-hook-form and zod validation CustomerModal

--- src/views/Customers/CustomerModal.tsx
+++ src/views/Customers/CustomerModal.tsx
@@ -10,7 +10,6 @@ import { Button } from "@/components/ui/button";
 import { useTranslation } from "react-i18next";
 import {
   Field,
-  FieldDescription,
   FieldError,
   FieldGroup,
   FieldLabel,
@@ -18,12 +17,35 @@ import {
   FieldSet,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import en from "@/i18n/locales/en.json";
+
+const customerSchema = z.object({
+  name: z.string().min(1, "nameRequired").max(60, "maxLength60"),
+  email: z.email("emailRequired"),
+  phone: z.string().max(15, "maxLength15").optional(),
+  identification: z.string().max(15, "maxLength15").optional(),
+  address: z.string().max(60, "maxLength60").optional(),
+});
+
+type CustomerFormData = z.infer<typeof customerSchema>;
+type ErrorFormKey = keyof typeof en.errorsForm;
 
 interface CustomerModalProps {
   isOpen: boolean;
   onClose: () => void;
   isEdit: boolean;
 }
+
+const defaultValues = {
+      name: "",
+      email: "",
+      phone: "",
+      identification: "",
+      address: "",
+    }
 
 const CustomerModal = ({
   isOpen,
@@ -32,9 +54,32 @@ const CustomerModal = ({
 }: CustomerModalProps): JSX.Element => {
   const { t } = useTranslation();
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    setError,
+  } = useForm<CustomerFormData>({
+    resolver: zodResolver(customerSchema),
+    defaultValues: /* isEditing
+      ? {
+          name: editingCustomer?.name,
+          email: editingCustomer?.email,
+          phone: editingCustomer?.phone || "",
+          id: editingCustomer?.id_number || "",
+          address: editingCustomer?.address || "",
+        }
+      :  */ defaultValues,
+  });
+
   const handleClose = () => {
-    // reset();
+    reset(defaultValues);
     onClose();
+  };
+
+  const onSubmit: SubmitHandler<CustomerFormData> = (formData) => {
+    console.log(formData);
   };
 
   return (
@@ -49,61 +94,69 @@ const CustomerModal = ({
             </DialogTitle>
           </DialogHeader>
 
-          <form>
-            <FieldGroup>
-              <FieldSet>
-                <FieldLegend>
-                  {t("customers.customerDescriptionModal")}
-                </FieldLegend>
+          <FieldGroup>
+            <FieldSet>
+              <FieldLegend>
+                {t("customers.customerDescriptionModal")}
+              </FieldLegend>
 
-                <FieldGroup className="grid grid-cols-1 md:grid-cols-2">
-                  <Field>
-                    <FieldLabel htmlFor="name">
-                      <span>{t("common.name")}</span>
-                      <span className="text-destructive">*</span>
-                    </FieldLabel>
-                    <Input id="name" />
-                    {/* <FieldError>Choose another username.</FieldError> */}
-                  </Field>
+              <FieldGroup className="grid grid-cols-1 md:grid-cols-2">
+                <Field>
+                  <FieldLabel htmlFor="name">
+                    <span>{t("common.name")}</span>
+                    <span className="text-destructive">*</span>
+                  </FieldLabel>
+                  <Input id="name" {...register("name")} />
+                  {errors.name && (
+                    <FieldError>
+                      {t(`errorsForm.${errors.name.message as ErrorFormKey}`)}
+                    </FieldError>
+                  )}
+                </Field>
 
-                  <Field>
-                    <FieldLabel htmlFor="email">
-                      <span>Email</span>
-                      <span className="text-destructive">*</span>
-                    </FieldLabel>
-                    <Input id="email" />
-                    {/* <FieldError>Choose another username.</FieldError> */}
-                  </Field>
+                <Field>
+                  <FieldLabel htmlFor="email">
+                    <span>Email</span>
+                    <span className="text-destructive">*</span>
+                  </FieldLabel>
+                  <Input id="email" {...register("email")} />
+                  {errors.email && (
+                    <FieldError>
+                      {t(`errorsForm.${errors.email.message as ErrorFormKey}`)}
+                    </FieldError>
+                  )}
+                </Field>
 
-                  <Field>
-                    <FieldLabel htmlFor="phone">
-                      <span>{t("customers.phone")}</span>
-                    </FieldLabel>
-                    <Input id="phone" />
-                  </Field>
+                <Field>
+                  <FieldLabel htmlFor="phone">
+                    <span>{t("customers.phone")}</span>
+                  </FieldLabel>
+                  <Input id="phone" {...register("phone")} />
+                </Field>
 
-                  <Field>
-                    <FieldLabel htmlFor="identification">
-                      <span>{t("customers.identification")}</span>
-                      <span className="text-destructive">*</span>
-                    </FieldLabel>
-                    <Input id="identification" />
-                  </Field>
+                <Field>
+                  <FieldLabel htmlFor="identification">
+                    <span>{t("customers.identification")}</span>
+                    <span className="text-destructive">*</span>
+                  </FieldLabel>
+                  <Input id="identification" {...register("identification")} />
+                </Field>
 
-                  <Field>
-                    <FieldLabel htmlFor="address">
-                      <span>{t("customers.address")}</span>
-                    </FieldLabel>
-                    <Input id="address" />
-                  </Field>
-                </FieldGroup>
-              </FieldSet>
-            </FieldGroup>
-          </form>
+                <Field>
+                  <FieldLabel htmlFor="address">
+                    <span>{t("customers.address")}</span>
+                  </FieldLabel>
+                  <Input id="address" {...register("address")} />
+                </Field>
+              </FieldGroup>
+            </FieldSet>
+          </FieldGroup>
 
           <DialogFooter>
-            <Button variant={"destructive"}>{t("common.cancel")}</Button>
-            <Button variant={"success"}>{t("common.save")}</Button>
+            <Button variant={"destructive"} onClick={handleClose}>{t("common.cancel")}</Button>
+            <Button variant={"success"} onClick={handleSubmit(onSubmit)}>
+              {t("common.save")}
+            </Button>
           </DialogFooter>
         </DialogContent>
       </Dialog>
